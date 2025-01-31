e9fdf35b3b792b0b29f6faf5d8943cb6e93aca00
annaistotts <ins17001@byui.edu>
2025-01-30 17:27:14 -0700

    removed spaces

--- Debugging/debugging.js
+++ Debugging/debugging.js
@@ -1,5 +1,4 @@
 const PI = 3.14;
-// let radius = 3;
 let area = 0;
 
 function circleArea(radius) {
@@ -9,6 +8,5 @@ function circleArea(radius) {
 
 area = circleArea(3);
 console.log("Area1:", area);
-// radius = 4;
 area = circleArea(4);
 console.log("Area2:", area);
